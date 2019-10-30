06dda3be7352b0013aba6c0ca8c117fff3f9b8e4
Michael Reeve <michael.s.reeve@gmail.com>
2019-10-30 08:14:51 -0400

    initial

---
+++ Parser/src/ParserGUI.java
@@ -0,0 +1,18 @@
+import BreezySwing.*;
+import javax.swing.*;
+public class ParserGUI extends GBFrame{
+	
+	
+	
+	
+	public ParserGUI() {
+		
+	}
+	
+	public static void main(String[] args) {
+		JFrame frm = new ParserGUI();
+		frm.setVisible(true);
+		frm.setSize(400,400);
+		frm.setTitle("Expression Parser");
+	}
+}
